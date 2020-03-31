99a47a521100e5a46c4644322cea12626445f379
Renato Centeno <hello@renatocenteno.com>
2020-03-30 21:11:12 -0500

    Config :construction:

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe, Get } from '@nestjs/common';
 import { UserRegisterDto } from './dto/user-register.dto';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-crendentials.dto';
@@ -19,5 +19,10 @@ export class AuthController {
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
   ): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialsDto);
+  }
+
+  @Get('/online')
+  test() {
+    return 'true!';
   }
 }
