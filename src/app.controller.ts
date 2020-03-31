5219abf63a0ae7cb0102d1ea766f68cf9441973d
Renato Centeno <hello@renatocenteno.com>
2020-03-31 05:26:03 -0500

    Endpoints for locations :construction:

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { getManager } from 'typeorm';
 
 @Controller('app')
 export class AppController {
   @Get('/districts/:province_id')
-  async districts(@Param() province_id: number) {
+  async districts(@Param('province_id', ParseIntPipe) province_id: number) {
     const entityManager = getManager();
     const rawData = await entityManager.query(
       `SELECT id, name FROM ubigeo_peru_districts WHERE province_id = ${province_id}`,
