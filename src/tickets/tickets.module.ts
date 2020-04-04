722f0086043ab61c2b0a099632bf3d69f5337b94
Renato Centeno <hello@renatocenteno.com>
2020-04-04 10:38:12 -0500

    Tickets :construction:

--- src/tickets/tickets.module.ts
+++ src/tickets/tickets.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TicketsController } from './tickets.controller';
 import { TicketsService } from './tickets.service';
-import { Ticket } from './ticket.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TicketRepository } from './ticket.repository';
 import { AuthModule } from 'src/auth/auth.module';
+import { TurnRepository } from 'src/companies/turn.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TicketRepository]), AuthModule],
+  imports: [
+    TypeOrmModule.forFeature([TicketRepository, TurnRepository]),
+    AuthModule,
+  ],
   controllers: [TicketsController],
   providers: [TicketsService],
 })
