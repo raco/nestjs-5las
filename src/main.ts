c2a794b8338338980b164cbf076ba3346b667755
Renato Centeno <hello@renatocenteno.com>
2020-04-05 13:25:23 -0500

    Security :construction:

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import * as config from 'config';
+import * as rateLimit from 'express-rate-limit';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const serverConfig = config.get('server');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.set('trust proxy', 1);
+  app.use(helmet());
   app.enableCors();
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
+
   await app.listen(process.env.PORT || serverConfig.port);
 }
 bootstrap();
