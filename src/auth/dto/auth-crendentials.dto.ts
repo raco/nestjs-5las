ed4a464371cac4bc367db8b8ca42561618999ce3
Renato Centeno <hello@renatocenteno.com>
2020-04-07 01:05:49 -0500

    Refactoring companies  :construction:

--- src/auth/dto/auth-crendentials.dto.ts
+++ src/auth/dto/auth-crendentials.dto.ts
@@ -16,8 +16,5 @@ export class AuthCredentialsDto {
   @IsString()
   @MinLength(8)
   @MaxLength(40)
-  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-    message: 'Password is too weak',
-  })
   password: string; // Mayuscula, numero y caracterespecial
 }
