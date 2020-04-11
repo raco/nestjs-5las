dcd3c30b04da04ee881a2bc311801ad55aecf636
Renato Centeno <hello@renatocenteno.com>
2020-04-11 03:00:53 -0500

    Mailing service :construction:

--- src/tickets/tickets.module.ts
+++ src/tickets/tickets.module.ts
@@ -5,11 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TicketRepository } from './ticket.repository';
 import { AuthModule } from 'src/auth/auth.module';
 import { TurnRepository } from 'src/companies/turn.repository';
+import { SharedModule } from 'src/shared/shared.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([TicketRepository, TurnRepository]),
     AuthModule,
+    SharedModule,
   ],
   controllers: [TicketsController],
   providers: [TicketsService],
